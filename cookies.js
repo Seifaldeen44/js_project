516ca4079c6d175442ee30d732a969947df99c36
Seifaldeen44 <saifhany65967@alexu.edu.eg>
2025-12-23 16:17:34 +0200

    first commit

---
+++ cookies.js
@@ -0,0 +1,24 @@
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const c = cookies[i].trim();
+    if (c.indexOf(name + "=") === 0) {
+      return c.substring(name.length + 1);
+    }
+  }
+  return "";
+}
+
+function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+}
